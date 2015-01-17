9ecea33e549789c3cfa7f0809988a619f42af76d
zjingye <710546910@qq.com>
2015-01-17 12:16:10 +0800

    增加https的支持

--- MyFramework/src/com/base/net/volley/toolbox/HttpClientStack.java
+++ MyFramework/src/com/base/net/volley/toolbox/HttpClientStack.java
@@ -33,162 +33,286 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpTrace;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.net.URI;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * An HttpStack that performs request over an {@link HttpClient}.
  */
 public class HttpClientStack implements HttpStack {
-    protected final HttpClient mClient;
+	protected final HttpClient mClient;
 
-    private final static String HEADER_CONTENT_TYPE = "Content-Type";
+	private final static String HEADER_CONTENT_TYPE = "Content-Type";
 
-    public HttpClientStack(HttpClient client) {
-        mClient = client;
-    }
+	public HttpClientStack(HttpClient client) {
+		mClient = client;
+		setClientHttps(mClient);
+	}
 
-    private static void addHeaders(HttpUriRequest httpRequest, Map<String, String> headers) {
-        for (String key : headers.keySet()) {
-            httpRequest.setHeader(key, headers.get(key));
-        }
-    }
+	/**
+	 * 对https请求支持
+	 * @param client
+	 */
+	private void setClientHttps(HttpClient client) {
+		
+		try {
+			KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			trustStore.load(null, null);
+			SSLSocketFactory sf = new SSLSocketFactoryEx(trustStore);
+			sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER); // 允许所有主机的验证
+			ClientConnectionManager conManager = client.getConnectionManager();
+			SchemeRegistry schReg = conManager.getSchemeRegistry();
+			if (schReg == null) {
+				schReg = new SchemeRegistry();
+			}
+			schReg.register(new Scheme("http", PlainSocketFactory
+					.getSocketFactory(), 80));
+			schReg.register(new Scheme("https", sf, 443));
 
-    @SuppressWarnings("unused")
-    private static List<NameValuePair> getPostParameterPairs(Map<String, String> postParams) {
-        List<NameValuePair> result = new ArrayList<NameValuePair>(postParams.size());
-        for (String key : postParams.keySet()) {
-            result.add(new BasicNameValuePair(key, postParams.get(key)));
-        }
-        return result;
-    }
+		} catch (KeyStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CertificateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KeyManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnrecoverableKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    @Override
-    public HttpResponse performRequest(Request<?> request, Map<String, String> additionalHeaders)
-            throws IOException, AuthFailureError {
-        HttpUriRequest httpRequest = createHttpRequest(request, additionalHeaders);
-        addHeaders(httpRequest, additionalHeaders);
-        addHeaders(httpRequest, request.getHeaders());
-        onPrepareRequest(httpRequest);
-        HttpParams httpParams = httpRequest.getParams();
-        int timeoutMs = request.getTimeoutMs();
-        // TODO: Reevaluate this connection timeout based on more wide-scale
-        // data collection and possibly different for wifi vs. 3G.
-        HttpConnectionParams.setConnectionTimeout(httpParams, 5000);
-        HttpConnectionParams.setSoTimeout(httpParams, timeoutMs);
-        return mClient.execute(httpRequest);
-    }
+	}
 
-    /**
-     * Creates the appropriate subclass of HttpUriRequest for passed in request.
-     */
-    @SuppressWarnings("deprecation")
-    /* protected */ static HttpUriRequest createHttpRequest(Request<?> request,
-            Map<String, String> additionalHeaders) throws AuthFailureError {
-        switch (request.getMethod()) {
-            case Method.DEPRECATED_GET_OR_POST: {
-                // This is the deprecated way that needs to be handled for backwards compatibility.
-                // If the request's post body is null, then the assumption is that the request is
-                // GET.  Otherwise, it is assumed that the request is a POST.
-                byte[] postBody = request.getPostBody();
-                if (postBody != null) {
-                    HttpPost postRequest = new HttpPost(request.getUrl());
-                    postRequest.addHeader(HEADER_CONTENT_TYPE, request.getPostBodyContentType());
-                    HttpEntity entity;
-                    entity = new ByteArrayEntity(postBody);
-                    postRequest.setEntity(entity);
-                    return postRequest;
-                } else {
-                    return new HttpGet(request.getUrl());
-                }
-            }
-            case Method.GET:
-                return new HttpGet(request.getUrl());
-            case Method.DELETE:
-                return new HttpDelete(request.getUrl());
-            case Method.POST: {
-                HttpPost postRequest = new HttpPost(request.getUrl());
-                postRequest.addHeader(HEADER_CONTENT_TYPE, request.getBodyContentType());
-                setEntityIfNonEmptyBody(postRequest, request);
-                return postRequest;
-            }
-            case Method.PUT: {
-                HttpPut putRequest = new HttpPut(request.getUrl());
-                putRequest.addHeader(HEADER_CONTENT_TYPE, request.getBodyContentType());
-                setEntityIfNonEmptyBody(putRequest, request);
-                return putRequest;
-            }
-            case Method.HEAD:
-                return new HttpHead(request.getUrl());
-            case Method.OPTIONS:
-                return new HttpOptions(request.getUrl());
-            case Method.TRACE:
-                return new HttpTrace(request.getUrl());
-            case Method.PATCH: {
-                HttpPatch patchRequest = new HttpPatch(request.getUrl());
-                patchRequest.addHeader(HEADER_CONTENT_TYPE, request.getBodyContentType());
-                setEntityIfNonEmptyBody(patchRequest, request);
-                return patchRequest;
-            }
-            default:
-                throw new IllegalStateException("Unknown request method.");
-        }
-    }
+	private static void addHeaders(HttpUriRequest httpRequest,
+			Map<String, String> headers) {
+		for (String key : headers.keySet()) {
+			httpRequest.setHeader(key, headers.get(key));
+		}
+	}
 
-    private static void setEntityIfNonEmptyBody(HttpEntityEnclosingRequestBase httpRequest,
-            Request<?> request) throws AuthFailureError {
-        byte[] body = request.getBody();
-        if (body != null) {
-            HttpEntity entity = new ByteArrayEntity(body);
-            httpRequest.setEntity(entity);
-        }
-    }
+	@SuppressWarnings("unused")
+	private static List<NameValuePair> getPostParameterPairs(
+			Map<String, String> postParams) {
+		List<NameValuePair> result = new ArrayList<NameValuePair>(
+				postParams.size());
+		for (String key : postParams.keySet()) {
+			result.add(new BasicNameValuePair(key, postParams.get(key)));
+		}
+		return result;
+	}
 
-    /**
-     * Called before the request is executed using the underlying HttpClient.
-     *
-     * <p>Overwrite in subclasses to augment the request.</p>
-     */
-    protected void onPrepareRequest(HttpUriRequest request) throws IOException {
-        // Nothing.
-    }
+	@Override
+	public HttpResponse performRequest(Request<?> request,
+			Map<String, String> additionalHeaders) throws IOException,
+			AuthFailureError {
+		HttpUriRequest httpRequest = createHttpRequest(request,
+				additionalHeaders);
+		addHeaders(httpRequest, additionalHeaders);
+		addHeaders(httpRequest, request.getHeaders());
+		onPrepareRequest(httpRequest);
+		HttpParams httpParams = httpRequest.getParams();
+		int timeoutMs = request.getTimeoutMs();
+		// TODO: Reevaluate this connection timeout based on more wide-scale
+		// data collection and possibly different for wifi vs. 3G.
+		HttpConnectionParams.setConnectionTimeout(httpParams, 5000);
+		HttpConnectionParams.setSoTimeout(httpParams, timeoutMs);
+		return mClient.execute(httpRequest);
+	}
 
-    /**
-     * The HttpPatch class does not exist in the Android framework, so this has been defined here.
-     */
-    public static final class HttpPatch extends HttpEntityEnclosingRequestBase {
+	/**
+	 * Creates the appropriate subclass of HttpUriRequest for passed in request.
+	 */
+	@SuppressWarnings("deprecation")
+	/* protected */static HttpUriRequest createHttpRequest(Request<?> request,
+			Map<String, String> additionalHeaders) throws AuthFailureError {
+		switch (request.getMethod()) {
+		case Method.DEPRECATED_GET_OR_POST: {
+			// This is the deprecated way that needs to be handled for backwards
+			// compatibility.
+			// If the request's post body is null, then the assumption is that
+			// the request is
+			// GET. Otherwise, it is assumed that the request is a POST.
+			byte[] postBody = request.getPostBody();
+			if (postBody != null) {
+				HttpPost postRequest = new HttpPost(request.getUrl());
+				postRequest.addHeader(HEADER_CONTENT_TYPE,
+						request.getPostBodyContentType());
+				HttpEntity entity;
+				entity = new ByteArrayEntity(postBody);
+				postRequest.setEntity(entity);
+				return postRequest;
+			} else {
+				return new HttpGet(request.getUrl());
+			}
+		}
+		case Method.GET:
+			return new HttpGet(request.getUrl());
+		case Method.DELETE:
+			return new HttpDelete(request.getUrl());
+		case Method.POST: {
+			HttpPost postRequest = new HttpPost(request.getUrl());
+			postRequest.addHeader(HEADER_CONTENT_TYPE,
+					request.getBodyContentType());
+			setEntityIfNonEmptyBody(postRequest, request);
+			return postRequest;
+		}
+		case Method.PUT: {
+			HttpPut putRequest = new HttpPut(request.getUrl());
+			putRequest.addHeader(HEADER_CONTENT_TYPE,
+					request.getBodyContentType());
+			setEntityIfNonEmptyBody(putRequest, request);
+			return putRequest;
+		}
+		case Method.HEAD:
+			return new HttpHead(request.getUrl());
+		case Method.OPTIONS:
+			return new HttpOptions(request.getUrl());
+		case Method.TRACE:
+			return new HttpTrace(request.getUrl());
+		case Method.PATCH: {
+			HttpPatch patchRequest = new HttpPatch(request.getUrl());
+			patchRequest.addHeader(HEADER_CONTENT_TYPE,
+					request.getBodyContentType());
+			setEntityIfNonEmptyBody(patchRequest, request);
+			return patchRequest;
+		}
+		default:
+			throw new IllegalStateException("Unknown request method.");
+		}
+	}
 
-        public final static String METHOD_NAME = "PATCH";
+	private static void setEntityIfNonEmptyBody(
+			HttpEntityEnclosingRequestBase httpRequest, Request<?> request)
+			throws AuthFailureError {
+		byte[] body = request.getBody();
+		if (body != null) {
+			HttpEntity entity = new ByteArrayEntity(body);
+			httpRequest.setEntity(entity);
+		}
+	}
 
-        public HttpPatch() {
-            super();
-        }
+	/**
+	 * Called before the request is executed using the underlying HttpClient.
+	 * 
+	 * <p>
+	 * Overwrite in subclasses to augment the request.
+	 * </p>
+	 */
+	protected void onPrepareRequest(HttpUriRequest request) throws IOException {
+		// Nothing.
+	}
 
-        public HttpPatch(final URI uri) {
-            super();
-            setURI(uri);
-        }
+	/**
+	 * The HttpPatch class does not exist in the Android framework, so this has
+	 * been defined here.
+	 */
+	public static final class HttpPatch extends HttpEntityEnclosingRequestBase {
 
-        /**
-         * @throws IllegalArgumentException if the uri is invalid.
-         */
-        public HttpPatch(final String uri) {
-            super();
-            setURI(URI.create(uri));
-        }
+		public final static String METHOD_NAME = "PATCH";
 
-        @Override
-        public String getMethod() {
-            return METHOD_NAME;
-        }
+		public HttpPatch() {
+			super();
+		}
 
-    }
+		public HttpPatch(final URI uri) {
+			super();
+			setURI(uri);
+		}
+
+		/**
+		 * @throws IllegalArgumentException
+		 *             if the uri is invalid.
+		 */
+		public HttpPatch(final String uri) {
+			super();
+			setURI(URI.create(uri));
+		}
+
+		@Override
+		public String getMethod() {
+			return METHOD_NAME;
+		}
+
+	}
+
+	class SSLSocketFactoryEx extends SSLSocketFactory {
+
+		SSLContext sslContext = SSLContext.getInstance("TLS");
+
+		public SSLSocketFactoryEx(KeyStore truststore)
+				throws NoSuchAlgorithmException, KeyManagementException,
+				KeyStoreException, UnrecoverableKeyException {
+			super(truststore);
+
+			TrustManager tm = new X509TrustManager() {
+
+				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+
+				@Override
+				public void checkClientTrusted(
+						java.security.cert.X509Certificate[] chain,
+						String authType)
+						throws java.security.cert.CertificateException {
+
+				}
+
+				@Override
+				public void checkServerTrusted(
+						java.security.cert.X509Certificate[] chain,
+						String authType)
+						throws java.security.cert.CertificateException {
+
+				}
+			};
+
+			sslContext.init(null, new TrustManager[] { tm }, null);
+		}
+
+		@Override
+		public Socket createSocket(Socket socket, String host, int port,
+				boolean autoClose) throws IOException, UnknownHostException {
+			return sslContext.getSocketFactory().createSocket(socket, host,
+					port, autoClose);
+		}
+
+		@Override
+		public Socket createSocket() throws IOException {
+			return sslContext.getSocketFactory().createSocket();
+		}
+	}
 }
